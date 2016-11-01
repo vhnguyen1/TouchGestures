8df7828446467008c15dcfe1e21cdb3a68851c9e
Nguyen <vnguyen629@student.cccd.edu>
2016-11-01 15:19:31 -0700

    Start of project

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/touchgestures/MainActivity.java
@@ -0,0 +1,74 @@
+package edu.orangecoastcollege.cs273.touchgestures;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements
+        GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
+
+    private ScrollView gesturesScrollView;
+
+    private TextView gesturesLogTextView;
+    private TextView singleTapTextView;
+    private TextView doubleTapTextView;
+    private TextView longPressTextView;
+    private TextView scrollTextView;
+    private TextView flingTextView;
+
+    private int singleTaps = 0, doubleTaps = 0, longPresses = 0, scrolls = 0, flings = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return false;
+    }
+}
